63751b80545fc5cebf266aecbe713acd076d37a6
huynhquynh-dev <huynhquynh-dev>
2021-04-05 15:12:53 +0700

    Added first source project

---
+++ src/test/java/com/lionhuynh/springbootoauth2sample/SpringBootOauth2SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionhuynh.springbootoauth2sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2SampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
